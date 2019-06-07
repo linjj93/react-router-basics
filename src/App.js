5a68bc24b8713e7897f03db50424061d1254ea92
linjj93 <linjj93@hotmail.com>
2019-06-07 15:44:17 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./App.css";
+
+function Home(props) {
+  console.log(props);
+  return <h1>Home</h1>;
+}
+
+function About() {
+  return <h1>About</h1>;
+}
+
+function Faq() {
+  return <h1>FAQ</h1>;
+}
+
+function Content({ text }) {
+  return <h1>{text}</h1>;
+}
+
+function Movie({ match }) {
+  const movieId = match.params.movieId;
+  const movies = [{ id: 1, title: "Endgame" }, { id: 2, title: "Avengers" }];
+  const movie = movies.find(m => m.id === Number(movieId));
+
+  return <h1>{movie && movie.title}</h1>; //render only if there is title
+}
 
 function App() {
+  const movieId = 2;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/faq">FAQ</Link>
+          </li>
+          <li>
+            <Link to="/content">Content</Link>
+          </li>
+          <li>
+            <Link to={`/movies/${movieId}`}>Movie</Link>
+          </li>
+        </nav>
+        <main>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/faq" component={Faq} />
+          <Route
+            path="/content"
+            render={() => <Content text="This is some text" />}
+          />
+          <Route path="/movies/:movieId" component={Movie} />
+        </main>
+      </div>
+    </Router>
   );
 }
 
